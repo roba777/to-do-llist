e0c9758e6747bfb86fc633a17cf28beaa29e282b
roba777 <roubii_@hotmail.com>
2021-12-20 11:20:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,72 @@
 const express= require('express');
-const { object } = require('webidl-conversions');
 const app= express()
 
 const db=require('./db')
 const Todo= require('./backend/todo')
-console.log(Todo)
+//console.log(Todo)
 
 
-
+app.use(express.json())
 
 
 app.get('/',(req,res)=>{
-    res.json("GIT ///is working")
+    res.json("GIT ///is working");
+});
+
+app.get('/tasks', (req,res)=>{
+    Todo.find({}, (err,data)=>{
+        if(err){
+            console.log('ERROR:', err);
+        }else{
+            res.json(data);
+        }
+    });
+});
+
+app.post('/tasks', (req,res)=>{
+    console.log('25:' ,req.body);
+
+    Todo.create(req.body, (err,newTask)=>{
+        if(err){
+            console.log('ERROR:', err);
+        }else{
+            res.status(201).json(newTask);
+        }
+    });
+});
+
+app.delete('/tasks/:id',(req,res)=>{
+    console.log('37:',req.params.id);
 })
 
+Todo.deleteOne({_id: req.params.id}, (err,deleteObj)=>{
+    if(err){
+        console.log("ERROR:", err);
+    }else{
+       deleteObj.deleteCount === 1
+       res.json("Delete one todo successfully")
+        res.status(404).json("This todo is not found");
+    }
+});
+
+
+app.put('/tasks/:id',(req,res)=>{
+    console.log('37:',req.params.id);
+})
+
+Todo.updateOne({_id: req.params.id},
+     {title:req.body.newTitle},
+     (err, updateObj)=>{
+    if(err){
+        console.log("ERROR:", err);
+        res.status(400).json(err)
+    }else{
+        console.log(updateObj)
+       deleteObj.deleteCount === 1
+       res.json("Delete one todo successfully")
+        res.status(404).json("This todo is not found");
+    }
+});
 
 
 
